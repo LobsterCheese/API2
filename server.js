314e07d4baf2117477eff64c2794cda81c045322
LobsterCheese <andersonsheu@hotmail.com>
2023-10-03 16:34:00 -0700

    gonna try this thinger out
    bitch

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const app = express();
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log(`Server is running on port ${process.env.PORT}.`);
-});
-
 
 app.use(express.static("./public"));
 
 app.get("/", function(req,res) {
-    res.sendFile(__dirname + "/src/pages/index.html");
+    res.sendFile(__dirname + "stuff1.html");
 });
 
 app.post("/", function(req,res) {
      console.log("you did a do");
+});
+
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log(`Server is running on port ${process.env.PORT}.`);
 });
